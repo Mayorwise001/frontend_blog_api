f3feaa92cadd038648a1d5dc897a1ade9a0e1a09
mayorwise001 <tomosorijoseph@yahoo.com>
2024-07-23 23:51:32 +0100

    created two routes

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import About from './components/about';
+import Login from './components/login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function Routed(){
+    return(
+        <Router>
+            <Routes>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/about' element={<About/>}/>
+            </Routes>
+        </Router>
+
+    )
+}
+export default Routed
---
+++ src/components/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About(){
+    return(
+        <>
+        <p>This is the About page</p>
+        </>
+    )
+}
+
+export default About;
